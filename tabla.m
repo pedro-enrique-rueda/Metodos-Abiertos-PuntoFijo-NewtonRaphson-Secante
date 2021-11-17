95583697efe4f5c1297aa016129ef1a77c0efd76
Pedro Enrique Rueda <67486183+pedro-enrique-rueda@users.noreply.github.com>
2021-11-17 12:24:22 -0300

    Update tabla.m

--- tabla.m
+++ tabla.m
@@ -3,7 +3,7 @@ function tab = tabla(raiz,error,iteracion)
   % Las siguientes lineas me imprimen en pantalla una tabla con los resultados
   
   %%% instale lo siguiente en la linea de comando:
-  %%% npk install https://github.com/apjanke/octave-tablicious/releases/download/v0.3.6/tablicious-0.3.6.tar.gz
+  %%% pkg install https://github.com/apjanke/octave-tablicious/releases/download/v0.3.6/tablicious-0.3.6.tar.gz
   pkg load tablicious %esto si es octave
   iteraciones = iteracion';
   raiz = raiz';
